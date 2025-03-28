53dd5a6b03ee0e01154b353e10868319beab3cfe
Vinicius <viniciuscavalcantidosreis@gmail.com>
2025-03-28 11:42:01 -0300

    Declaração de variáveis

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+// Declaração e variaveis
+
+var nome ="Fiap";
+console.log(nome)
+
+let idade =18;
+console.log(idade)
+
+const sobrenome ="Fiapinho";
+console.log(sobrenome)
+
+// undefined
+
+let exemplo;
+console.log(exemplo)
+
+// nula
+
+let exemplo1 =null
+console.log(exemplo1)
+
+// tipos de variaveis
+
